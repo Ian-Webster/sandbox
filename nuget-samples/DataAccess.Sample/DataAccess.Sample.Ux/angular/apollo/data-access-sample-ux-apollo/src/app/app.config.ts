43c237e42573d5c988f46a8cd33d8237b5fcfab5
Webster <i_web_99@yahoo.com>
2024-01-10 00:08:08 +0000

    #25 basic material theme

--- nuget-samples/DataAccess.Sample/DataAccess.Sample.Ux/angular/apollo/data-access-sample-ux-apollo/src/app/app.config.ts
+++ nuget-samples/DataAccess.Sample/DataAccess.Sample.Ux/angular/apollo/data-access-sample-ux-apollo/src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { graphqlProvider } from './graphql.provider';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideHttpClient(withFetch()), 
-		provideRouter(routes),
-		provideHttpClient(),
-		graphqlProvider,
-	] 
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideHttpClient(),
+    graphqlProvider,
+    provideAnimations()
+] 
 };
